dd48ec98c58c046f35ee4d7e158f0c2caf640e79
ascalmon <ascalmon@gmail.com>
2019-02-24 20:22:19 -0300

    Delete contact implemented with back end jsonplaceholder

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -3,7 +3,7 @@ import TextInputGroup from '../layout/TextInputGroup';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addContact } from '../../actions/contactActions';
-import uuid from 'uuid';
+//import uuid from 'uuid';
 
 
 class AddContact extends Component {
@@ -36,7 +36,7 @@ class AddContact extends Component {
     }
 
     const newContact = {
-      id: uuid(),    // Created due to the lack of a backend to provide a real id.
+      //id: uuid(),    // Created due to the lack of a backend to provide a real id.
       name,
       email,
       phone
